dfb0585bb9cd1ddaa658ed2c8a4cd0b7ef3dfb62
João Paulo <joaoroque317@gmail.com>
2022-02-18 02:06:51 -0300

    updates

--- Gerador de senha.py
+++ Gerador de senha.py
@@ -6,7 +6,7 @@ simbolos = ascii_lowercase + digits + ascii_uppercase + punctuation
 
 simbolos_aleatorios = random.SystemRandom()
 
-quant = int(input("Degite a quantidade de caracteres para a senha: "))
+quant = int(input("Informe a quantidade de caracteres para a senha: "))
 
 senha = "".join(simbolos_aleatorios.choice(simbolos) for i in range(quant))
 horario = strftime('%d-%m-%y %H:%M:%S', localtime())
@@ -16,6 +16,6 @@ print("Senha gerada no horário", horario, file=arquivo)
 print(senha, file=arquivo) 
 arquivo.close()
 
-print("Senha salva no arquivo")
+print("Senha salva")
 
 
