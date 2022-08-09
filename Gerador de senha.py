2a75e32dcba24d9a47c5b7df748dff9fdbbf333d
João Paulo <joaoroque317@gmail.com>
2022-08-09 05:47:22 -0300

    Upgrade

--- Gerador de senha.py
+++ Gerador de senha.py
@@ -2,20 +2,24 @@ import random
 from string import digits, punctuation, ascii_uppercase, ascii_lowercase
 from time import strftime, localtime
 
-simbolos = ascii_lowercase + digits + ascii_uppercase + punctuation
-
-simbolos_aleatorios = random.SystemRandom()
-
-quant = int(input("Informe a quantidade de caracteres para a senha: "))
-
-senha = "".join(simbolos_aleatorios.choice(simbolos) for i in range(quant))
-horario = strftime('%d-%m-%y %H:%M:%S', localtime())
-
-arquivo = open('senhas.txt', 'a')
-print("Senha gerada no horário", horario, file=arquivo)
-print(senha, file=arquivo) 
-arquivo.close()
-
-print("Senha salva")
+tam = int(input("Informe a quantidade de caracteres para a senha: "))
 
 
+def Senha(tamanho):
+    simbolos = ascii_lowercase + digits + ascii_uppercase + punctuation
+    simbolos_aleatorios = random.SystemRandom()
+    senha = "".join(simbolos_aleatorios.choice(simbolos) for i in range(tamanho))
+    horario = strftime('%d-%m-%y %H:%M:%S', localtime())
+
+    arquivo = open('senhas.txt', 'a')
+    print("-------------------------------------------", file=arquivo)
+    print("Senha gerada no horário", horario, file=arquivo)
+    print(senha, file=arquivo) 
+    arquivo.close()
+    print("Senha salva")
+
+
+while tam <= 5: 
+        tam = int(input("Informe a quantidade de caracteres para a senha: "))
+    
+Senha(tam)
